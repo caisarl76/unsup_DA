3ec6e953538dc9e4435f7ee622c360c5a5aff651
caisarl76 <caisarl76@gmail.com>
2021-04-14 10:18:33 +0900

    add shell making code

---
+++ get_test_sh.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+from collections import OrderedDict
+from os.path import join as join
+import torch
+from torch.utils import data as util_data
+from torchvision import transforms
+
+domain_dict = {'r': 'RealWorld', 'a': 'Art', 'c': 'Clipart', 'p': 'Product'}
+
+base = 'CUDA_VISIBLE_DEVICES=4 python test.py --data-root /data/OfficeHomeDataset_10072016 --model-name resnet50dsbn --model-path /result/rot_ssl/resnet50/'
+
+dirs = ['rot_sup/resnet50',
+        'byol_finetune',
+        'rot_ssl/byol',
+        'rot_ssl/resnet50',
+        'rot_ssl_byol/resnet50',
+        'rot_sup/resnet50'
+        ]
+
+
+def main():
+    for dir in dirs:
+        path = join('/result', dir)
+        exp_list = os.listdir(path)
+        for exp in exp_list:
+            src, trg = exp.split('_')
+            src = domain_dict[src]
+            trg = domain_dict[trg]
+            
+
+if __name__ == '__main__':
+    main()
