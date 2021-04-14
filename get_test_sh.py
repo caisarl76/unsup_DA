87691ad436e5c9d203f47b19030384bafdb79fea
caisarl76 <caisarl76@gmail.com>
2021-04-14 10:21:02 +0900

    add shell making code

--- get_test_sh.py
+++ get_test_sh.py
@@ -11,7 +11,7 @@ domain_dict = {'r': 'RealWorld', 'a': 'Art', 'c': 'Clipart', 'p': 'Product'}
 base = 'CUDA_VISIBLE_DEVICES=4 python test.py --data-root /data/OfficeHomeDataset_10072016 --model-name resnet50dsbn --model-path /result/rot_ssl/resnet50/'
 
 dirs = ['rot_sup/resnet50',
-        'byol_finetune',
+        # 'byol_finetune',
         'rot_ssl/byol',
         'rot_ssl/resnet50',
         'rot_ssl_byol/resnet50',
@@ -25,6 +25,7 @@ def main():
         exp_list = os.listdir(path)
         for exp in exp_list:
             print(exp)
+
             src, trg = exp.split('_')
             src = domain_dict[src]
             trg = domain_dict[trg]
