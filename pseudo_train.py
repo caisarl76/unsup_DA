12e66281f9332d6c024e48b162e84ca09d2eec7c
caisarl76 <caisarl76@gmail.com>
2021-04-21 13:06:43 +0900

    add freeze codes

--- pseudo_train.py
+++ pseudo_train.py
@@ -206,7 +206,7 @@ def main():
     trg_num = domain_dict[args.trg_domain]
 
     ###################### train teacher model ######################
-    t_path = join(args.teacher_root, '%s_%s/' % (args.trg_domain, args.src_domain))
+    t_path = join(args.teacher_root, '%s_%s/' % (args.src_domain, args.trg_domain))
 
     if (args.save_root):
         save_root = args.save_root
@@ -214,7 +214,7 @@ def main():
 
     teacher = get_model(args.model_name, num_classes=65, in_features=0, num_domains=2, pretrained=True)
 
-    t2_path = join(t_path, 'stage2/best_resnet50dsbn+None+i0_%s2%s.pth' % (args.trg_domain, args.src_domain))
+    t2_path = join(t_path, 'stage2/best_resnet50dsbn+None+i0_%s2%s.pth' % (args.src_domain, args.trg_domain))
     print(t2_path)
     # if not os.path.isfile(t2_path) or args.train_teacher:
     if not os.path.isfile(t2_path):
